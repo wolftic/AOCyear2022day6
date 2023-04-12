bdf26b48a13acf658e322990bd812408f0c95b61
Lorenzo Kramer <lorenzokramertje@gmail.com>
2023-04-12 20:56:31 +0200

    Finished part 1

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,47 @@
 "use strict";
-console.log("Hello world!");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const fileName = process.argv.pop();
+const filePath = path.join(__dirname, "../data/", fileName + ".txt");
+const signal = fs.readFileSync(filePath, "utf-8");
+for (let i = 4; i < signal.length; i++) {
+    let isValid = true;
+    let text = "";
+    for (let j = i - 4; j <= i; j++) {
+        if (text.includes(signal[j])) {
+            isValid = false;
+            break;
+        }
+        text += signal[j];
+    }
+    if (!isValid) {
+        continue;
+    }
+    console.log(text);
+    console.log(i);
+    break;
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
 import * as fs from "fs";
+import * as path from "path";
 
-console.log("Hello world!");
+const fileName = process.argv.pop();
+const filePath = path.join(__dirname, "../data/", fileName + ".txt");
+
+const signal = fs.readFileSync(filePath, "utf-8");
+
+for (let i = 4; i < signal.length; i++) {
+  let isValid = true;
+  let text = "";
+
+  for (let j = i - 4; j <= i; j++) {
+    if (text.includes(signal[j])) {
+      isValid = false;
+      break;
+    }
+
+    text += signal[j];
+  }
+
+  if (!isValid) {
+    continue;
+  }
+
+  console.log(text);
+  console.log(i);
+  break;
+}
